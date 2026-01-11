c007840ff54e98e947d38e140107f8123b9a73c3
sylvain kadjo <sylvainka12@gmail.com>
2026-01-11 22:42:24 +0400

    Project config
    - add github actions workflows
    - add crates for projet CI pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    let mut name = String::new();
+    name.push('a');
+    name.push('b');
+
+    println!("{}", name);
     println!("Hello, world!");
+
+    // FIXME: Implement the functions
+    let s = todo!();
 }
