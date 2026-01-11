711c6d10b02b256d71028101f3536df593107ed9
sylvain kadjo <sylvainka12@gmail.com>
2026-01-11 22:50:05 +0400

    clean main entry

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let mut name = String::new();
-    name.push('a');
-    name.push('b');
-
-    println!("{}", name);
     println!("Hello, world!");
-
-    // FIXME: Implement the functions
-    let s = todo!();
 }
